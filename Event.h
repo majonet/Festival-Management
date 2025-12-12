81be29ff9442230f1f7000801f538e6a8537ae65
majonet <salamspacex@gmail.com>
2025-12-12 19:04:28 +0400

    event hpp write

--- Event.h
+++ Event.h
@@ -1,1 +1,23 @@
-hello 
+// Event.h
+// تعریف کلاس Event با ویژگی‌های:
+//  eventName، eventID، eventType، capacity، registeredParticipants
+#include<string>
+#pragma once
+#include <iostream>
+using namespace std;
+#include "Participant.h"
+#include<map>
+enum Etype {ev1,ev2,ev2};
+
+class Event{
+protected:
+   string eventName;
+   int eventID;
+   Etype eventType;
+   int capacity;
+   map<int, string> registeredParticipants;
+public:
+   Event(){};
+   ~Event(){};
+   
+};
---
+++ Participant.h
@@ -0,0 +1,9 @@
+#include<string>
+using namespace std;
+#pragma once
+//  name، participantID، registeredEvents
+class Participant{
+   string name;
+   int participantID;
+   
+};
