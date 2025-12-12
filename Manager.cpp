b8c2494c1e97c3d8f1a3965514bbef018a5bbc62
majonet <salamspacex@gmail.com>
2025-12-12 20:26:09 +0400

    add registerParticipantToEvent manager cpp

--- Manager.cpp
+++ Manager.cpp
@@ -7,3 +7,11 @@ void Manager::addEvent(Event m){
 void Manager::addParticipant(Participant m){
     cont_Participants.insert({m.participantID, m});
 };
+void Manager::registerParticipantToEvent(int participantID,int eventID){
+    Event se_event;
+    se_event=cont_Events[eventID];
+    if (se_event.registeredParticipants.count(participantID)) {
+    cout << "participant ID  find";
+} else {
+    cout << "participant ID not found";}
+}
